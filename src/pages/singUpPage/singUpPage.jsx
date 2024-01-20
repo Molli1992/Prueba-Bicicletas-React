17bc20339f243e8a47ba70e4f9b560debd8c9bbe
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-01-20 20:32:01 -0300

    deploy 3

--- src/pages/singUpPage/singUpPage.jsx
+++ src/pages/singUpPage/singUpPage.jsx
@@ -190,7 +190,7 @@ function SingUpPage() {
                     <img
                       src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
                       class="img-fluid"
-                      alt="Sample image"
+                      alt="Error"
                     />
                   </div>
                 </div>
