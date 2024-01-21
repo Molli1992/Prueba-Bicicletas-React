8908bc11c1f39c837242622e80e8a62bd4a2cf78
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-01-20 23:43:42 -0300

    deploy 2

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,44 +6,26 @@ import Swal from "sweetalert2";
 const axiosUrl = process.env.REACT_APP_AXIOS_URL;
 
 export function getCarts() {
-  try {
-    const cookie = new Cookies();
-    const cookieID = cookie.get("id");
+  const cookie = new Cookies();
+  const cookieID = cookie.get("id");
 
-    return async function (dispatch) {
-      let results = await axios.get(`${axiosUrl}/api/cart/${cookieID}`);
+  return async function (dispatch) {
+    let results = await axios.get(`${axiosUrl}/api/cart/${cookieID}`);
 
-      return dispatch({
-        type: GET_CART,
-        payload: results.data,
-      });
-    };
-  } catch (error) {
-    Swal.fire({
-      title: "Error!",
-      text: error,
-      icon: "error",
-      confirmButtonText: "Ok",
+    return dispatch({
+      type: GET_CART,
+      payload: results.data,
     });
-  }
+  };
 }
 
 export function getProducts() {
-  try {
-    return async function (dispatch) {
-      let results = await axios.get(axiosUrl + "/api/products");
+  return async function (dispatch) {
+    let results = await axios.get(axiosUrl + "/api/products");
 
-      return dispatch({
-        type: GET_PRODUCTS,
-        payload: results.data,
-      });
-    };
-  } catch (error) {
-    Swal.fire({
-      title: "Error!",
-      text: error,
-      icon: "error",
-      confirmButtonText: "Ok",
+    return dispatch({
+      type: GET_PRODUCTS,
+      payload: results.data,
     });
-  }
+  };
 }
